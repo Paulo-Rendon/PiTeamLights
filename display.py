9ab78585b86b6e39491e027f36657ce9c4cea5fa
Paulo Rendon Jr <60458159+Paulo-Rendon@users.noreply.github.com>
2022-11-23 16:45:21 -0800

    Worked on displaying the games to the screen

--- display.py
+++ display.py
@@ -1,27 +1,59 @@
-# I am going to use Kivy to do the display bc it offers beutiful design
-# and seems very capatable when it comes to working on the raspberry Pi and even
+# THIS IS FOR EDUCATIONAL PURPOSES ONLY, GOOGLE DOES NOT GIVE ME OR ANYONE PERMISSION
+# FROM DATA SCRAPPING GOOGLE SEARCH RESULTS TO MAKE COMMERCIAL PROFIT
+
+# I am going to use Kivy to do the display bc it offers beautiful design
+# and seems very capable when it comes to working on the raspberry Pi and even
 # gives me the option to export to android in the future if I wish (prob pay for API access
 # before I decide to export to android)
 
-#This code is following a tutorial by Python Simplified on YT https://youtu.be/YDp73WjNISc
+# Created by Paulo Rendon Jr: Sep. 19th 2022
+# Last updated by Paulo Rendon Jr: Oct. 5th 2022
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
+from kivy.clock import Clock
+from kivy.lang import Builder
 import gameHandler
+import threading
+from kivy.uix.image import Image
+from dataScraper import updateFlags
 from functools import partial
 
-class SayHello(App):
+debug = True
+Builder.load_string('''
+<GreyGrid>
+    canvas.before:
+        Color:
+            rgba: 0.5, 0.5, 0.5, 1
+        RoundedRectangle:
+            size: self.size
+            pos: self.pos
+            radius: [(40, 40), (40, 40), (40, 40), (40, 40)]
+''')
+
+class GreyGrid(GridLayout):
+    pass
+#The main class obj created for display
+class appDisplay(App):
+
+    # This function is the default screen constructor
     def build(self):
+
+        # Builds the gamehandler obj
+        self.gamehandler = gameHandler.gameHandler(debug)
+        # For holding all gamecontainers
+        self.gameContainers = []
         self.window = BoxLayout()
         self.window.orientation = "vertical"
         self.grid = GridLayout()
         self.grid.cols = 2
+        self.grid.spacing = 5
+        self.grid.padding = 10
         #side margin 60%, top bottom margin 70%
         self.grid.size_hint = (0.6, 0.7)
         self.grid.pos_hint = {"center_x":0.5, "center_y":0.5}
@@ -36,28 +68,19 @@ class SayHello(App):
                         #This makes the button color more close to color specified
                         background_normal=""
                         )
-        self.button.bind(on_press=self.callback)
+        self.button.bind(on_press=self.addGameBtn)
         self.window.add_widget(self.button)
-        self.grid.add_widget(Image(source = "disgust_bird.jpg"))
-        self.greeting = Label(
-                        text="Hello World!",
-                        font_size = 18,
-                        color="#00FFCE"
-                        )
-        self.grid.add_widget(self.greeting)
-        self.user = TextInput(
-                    multiline=False,
-                    padding_y = (20,20),
-                    size_hint=(1, 0.5)
-                    )
-
-        self.grid.add_widget(self.user)
-
         self.window.add_widget(self.grid)
+
+        #Schedule updateScreen to run every minute
+        #The lambda is to specify that updateScreen takes no arguments and the 1 provided is for the Clock obj
+        Clock.schedule_interval(lambda dt: self.updateScreen(), 60)
+
 
         return self.window
 
-    def callback(self, instance):
+    # This is the popup object for when the add game button is pressed
+    def addGameBtn(self, instance):
         addGameLayout = GridLayout()
         addGameLayout.cols = 2
         yourTeamText = Label(text='Your Team:',
@@ -85,7 +108,6 @@ class SayHello(App):
                         #This makes the button color more close to color specified
                         background_normal=""
                         )
-        #buttonCallBack = lambda yourTeam, opTeam: self.addGameToScreen(yourTeam, opTeam)
         subButton.bind(on_press = self.addGameToScreen)
         exitButton = Button(
                         text="close",
@@ -97,26 +119,117 @@ class SayHello(App):
                         background_normal=""
                         )
         exitButton.bind(on_press = lambda arg : self.addGame.dismiss())
+
+        # add defined objects to layout
         addGameLayout.add_widget(yourTeamText)
         addGameLayout.add_widget(opTeamText)
         addGameLayout.add_widget(self.yourTeam)
         addGameLayout.add_widget(self.opTeam)
         addGameLayout.add_widget(subButton)
         addGameLayout.add_widget(exitButton)
+
         #self.greeting.text = "Hello " + self.user.text + "!"
         self.addGame = Popup(title='Add Game',
                       content=addGameLayout,
                       size_hint=(None, None), size=(400, 400))
-
         self.addGame.open()
 
+
+    # This function adds the game container to the screen
+    # Should be called by the main thread since it includes GUI altering
     def addGameToScreen(self, instance):
-        gameHandler.addingGame(self.yourTeam.text, self.opTeam.text)
-        self.addGame.dismiss()
+        if(debug):
+            print("Adding game between " + self.yourTeam.text + " and " + self.opTeam.text)
+
+        # Add game to game handler
+        vals = self.gamehandler.addingGame(self.yourTeam.text, self.opTeam.text)
+        self.addGame.dismiss()  #close popup
+
+        # Change grid layout to fit more reasonably when displayed
+        self.grid.cols = len(self.gamehandler.games) // 2
+        if (len(self.gamehandler.games) % 2):
+            self.grid.cols += 1
+
+        # Build a game Container (setup for dubugging using hard values)
+        gameContainer = GreyGrid()
+        gameContainer.rows = 2
+
+        yourTeamText = Label(text=str(self.yourTeam.text),
+                             font_size=14,
+                             size_hint=(0.5, 0.1))
+        opTeamText = Label(text=str(self.opTeam.text),
+                           font_size=14,
+                           size_hint=(0.5, 0.1))
+        gameContainer.yourTeamScore = Label(text=str(vals[0]),
+                              font_size=14,
+                              size_hint=(0.5, 0.1))
+        gameContainer.opTeamScore = Label(text=str(vals[1]),
+                            font_size=14,
+                            size_hint=(0.5, 0.1))
+        gameContainer.time = Label(text=str(vals[2]),
+                     font_size=14,
+                     size_hint=(0.5, 0.1))
+
+        dash = Label(text="-",
+                     font_size=21,
+                     size_hint=(0.5, 0.1))
+
+        # Add objects to layout
+        gameContainer.add_widget(yourTeamText)
+        gameContainer.add_widget(gameContainer.time)
+        gameContainer.add_widget(opTeamText)
+        gameContainer.add_widget(gameContainer.yourTeamScore)
+        gameContainer.add_widget(dash)
+        gameContainer.add_widget(gameContainer.opTeamScore)
+
+        # Add layout to gameContainers list and to grid
+        self.gameContainers.append(gameContainer)
+        self.grid.add_widget(gameContainer)
         return
 
+    # This function can be handled by the child thread
+    #def updateGames(self):
+        #this will run when a game is made, and will run till no games exist
+        #this is where game handler will be stored?
+     #   if (debug):
+      #      print("How Many Games: ")
+       #     print(len(self.gamehandler.games))
+        #while(len(self.gamehandler.games)):
+         #   for game in self.gamehandler.games:
+          #      if(debug):
+           #         print("Creating temp game!")
+
+                #self.gameContainers[0].yourTeamScore.text = game.myScore
+                #self.gameContainers[0].children["3"].text = "3"
+                #game.myTeam
+                #game.myScore
+                #game.opTeam
+                #game.opScore
+                #game.time
+                # I can call this on everything (Used for lights section of project (to come))
+                #game.updated[updateFlags.MYTEAMSCORED]
+            #gameHandler.sleep(60)
+        #return
+
+
+    # This function should be added to the kivy scheduler to be called every minute
+    def updateScreen(self):
+        games = self.gamehandler.returnGames()
+        for i in range(len(games)):
+
+            # Need to str cast these ints
+            self.gameContainers[i].yourTeamScore.text = str(games[i].myScore)
+            self.gameContainers[i].opTeamScore.text = str(games[i].opScore)
+            self.gameContainers[i].time.text = games[i].time
+            print(games[i].time)
+            print(self.gameContainers[i])
+        print("Been a minute!")
+
+        return
+
+
 def main():
-    SayHello().run()
+    appDisplay().run()
 
-
-main()
+if __name__ == "__main__":
+    main()
--- gameHandler.py
+++ gameHandler.py
@@ -1,72 +1,85 @@
+# THIS IS FOR EDUCATIONAL PURPOSES ONLY, GOOGLE DOES NOT GIVE ME OR ANYONE PERMISSION
+# FROM DATA SCRAPPING GOOGLE SEARCH RESULTS TO MAKE COMMERCIAL PROFIT
+
+# This class is for the game handler, which will be in charge of
+# updating the multiple games scores by calling the data scraper class
+# as well as handle all of the threads that are assigned to each game
+
+# Created by Paulo Rendon Jr: July 25th 2022
+# Last updated by Paulo Rendon Jr: Oct. 5th 2022
+
 import dataScraper
 import threading
 from time import time, sleep
 
-games = []
-threads = []
-halftimes = [20, 5]
-debug = True
+class gameHandler:
+    games = []
+    threads = []
+    halftimes = [20, 5]
+    debug = False
 
-def convert_string(temp):
-    for i in range(len(temp) - 1):
-        if temp[i] == ' ':
-            temp = temp[0:i] + '+' + temp[i + 1:len(temp)]
-    return temp
+    def __init__(self, debug):
+        self.debug = debug
+        if(debug):
+            print("Game Handler built in debug mode")
 
-def gameRunning(myTeam, opTeam):
-    game1 = dataScraper.game(myTeam, opTeam, threading.current_thread().ident)
-    games.append(game1)
-    half_pause = 0
-    print("\nTime: " + game1.time)
-    print(game1.myTeam + ": " + str(game1.myScore))
-    print(game1.opTeam + ": " + str(game1.opScore))
-    while(game1.updated[dataScraper.updateFlags.FULLTIME.value] != 1):
-        if(game1.updated[dataScraper.updateFlags.OPTEAMSCORED.value]):
-            print(game1.opTeam + " Scored :(")
-        if(game1.updated[dataScraper.updateFlags.MYTEAMSCORED.value]):
-            print(game1.myTeam + " Scored :)")
+    def convert_string(self, temp):
+        for i in range(len(temp) - 1):
+            if temp[i] == ' ':
+                temp = temp[0:i] + '+' + temp[i + 1:len(temp)]
+        return temp
 
-        #wait then update
-        sleep(60 - time() % 60)
-        if(game1.updated[dataScraper.updateFlags.HALFTIME.value]):
-            print("Halftime")
-            sleep(halftimes[half_pause]*60)
-            half_pause = 1
-        updated = game1.update()
-        print("Time: " + game1.time)
-        print(game1.myTeam + ": " + str(game1.myScore))
-        print(game1.opTeam + ": " + str(game1.opScore))
+    def gameRunning(self, myTeam, opTeam):
+        game1 = dataScraper.game(myTeam, opTeam, threading.current_thread().ident, self.debug)
+        self.games.append(game1)
+        half_pause = 0
+        if(self.debug):
+            print("\nTime: " + game1.time)
+            print(game1.myTeam + ": " + str(game1.myScore))
+            print(game1.opTeam + ": " + str(game1.opScore))
+        while(game1.updated[dataScraper.updateFlags.FULLTIME.value] != 1):
+            if(game1.updated[dataScraper.updateFlags.OPTEAMSCORED.value] and self.debug):
+                print(game1.opTeam + " Scored :(")
+            if(game1.updated[dataScraper.updateFlags.MYTEAMSCORED.value] and self.debug):
+                print(game1.myTeam + " Scored :)")
 
-    print("The game between " + game1.myTeam + " and " + game1.opTeam + " is over!")
+            #wait then update
+            sleep(60 - time() % 60)
+            if(game1.updated[dataScraper.updateFlags.HALFTIME.value]):
+                if(self.debug):
+                    print("Halftime")
+                sleep(self.halftimes[half_pause]*60)
+                half_pause = 1
+            game1.update()
+            if(self.debug):
+                print("Time: " + game1.time)
+                print(game1.myTeam + ": " + str(game1.myScore))
+                print(game1.opTeam + ": " + str(game1.opScore))
 
-    #Wait an hour bf removing the results of a finished game
-    sleep(60 * 60)
-    index = 0
-    for game in games:
-        index+=1
-        if (game.tID == threading.current_thread().ident):
-            games.remove(game)
-            if (debug):
-                print("Leaving game " + str(index))
+        if(self.debug):
+            print("The game between " + game1.myTeam + " and " + game1.opTeam + " is over!")
 
+        #Wait an hour bf removing the results of a finished game
+        sleep(60 * 60)
 
-def addingGame(myTeam, opTeam):
-    #command = input("Is there more teams to add? (Y/N)")
-    #while(command == "Y"):
+    def addingGame(self, myTeam, opTeam):
+        #command = input("Is there more teams to add? (Y/N)")
+        #while(command == "Y"):
         #myTeam = input("My Team:\n")
         #opTeam = input("The Opposing Team:\n")
         #myTeam = convert_string(myTeam)
         #opTeam = convert_string(opTeam)
-        t = threading.Thread(target=gameRunning, args=(myTeam, opTeam,))
+        gameSize = len(self.games)
+        t = threading.Thread(target=self.gameRunning, args=(myTeam, opTeam,))
         t.daemon = True
         t.start()
-        threads.append(t)
-        #command = input("Is there more teams to add? (Y/N)")
+        self.threads.append(t)
+        while(gameSize +1 != len(self.games)):
+            continue
+            #Wait till the thread adds the game to the "games" list
+        gameSize+=1
+        vals = [str(self.games[-1].myScore), str(self.games[-1].opScore), str(self.games[-1].time)]
+        return vals
 
-    #print("The program will run until the user cancels the program")
-    #command = input("Do you want to end the program? Y/N")
-    #while(command != "Y")
-        for thread in threads:
-            if not thread.is_alive():
-                threads.remove(thread)
-        pass
+    def returnGames(self):
+        return self.games
